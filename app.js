aa4a91c8931d0b8fd078f58cd4942d46f80047e5
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-04-19 20:34:19 +0530

    Initialize mongoose db connection

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/**
+ * Learning reference: 
+ * https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/skeleton_website
+ * https://github.com/mdn/express-locallibrary-tutorial
+ */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +13,17 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Setup mongoose connection
+var mongoose = require('mongoose');
+var dev_db_url='mongodb://localhost:27017/studentdb?readPreference=primary&ssl=false';
+var db = mongoose.connection;
+mongoose.connect(dev_db_url);
+// mongoose.Promise = global.Promise; //need to understand
+var db= mongoose.connection;
+db.on('error', console.error.bind(console,'MongoDB connection error'));
+db.on('connected', ()=>{console.log('DB Connected')});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
